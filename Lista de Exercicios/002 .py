c62d48641c26a109f7cc0bed76883c368fcbca00
Davidson Sousa <127262750+DavidsonSous@users.noreply.github.com>
2023-04-08 09:35:12 -0300

    Delete Lista de Exercicios directory

--- Lista de Exercicios/002 .py
+++
@@ -1,4 +0,0 @@
-# Criar um código que responda: 'Olá, ... (seu nome)'
-nome = input('Digite seu nome: ')
-print('                 ')
-print(f'Olá, {nome}, é um prazer te conhecer. Seja bem vindo(a)!')
